ec20814c4589b159459e1deabb2df99b711a2828
Caio Francolin <caiofrancolin@gmail.com>
2021-06-01 17:58:01 -0300

    Alterações na função para tratar imagens

--- optimize.js
+++ optimize.js
@@ -1,9 +1,26 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const S3 = new AWS.S3();
 
-module.exports.handle = async (event) => {
+module.exports.handle = async ({ Records: records }, context) => {
   try {
-    
+    await Promise.all(records.map( record => {
+      const { key } = record.s3.object;
+
+      const image = await S3.getObject({
+        Bucket: Process.env.bucket,
+        Key: key,
+      }).promise();
+
+      
+    }));
+  
+    return {
+      statusCode: 301,
+      body: {}
+    }
+  } catch (err) {
+    return err;
   }
 };
